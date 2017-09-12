33489cb95c7b86f2aaad4c03d294f417828860b6
976251185 <976251185@qq.com>
2017-09-12 19:06:08 +0800

    Add user's picture by gravatar.

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -2,6 +2,7 @@ import unittest
 import time
 from app import create_app, db
 from app.models import User, AnonymousUser, Role, Permission
+from datetime import datetime
 
 
 class UserModelTestCase(unittest.TestCase):
@@ -114,3 +115,38 @@ class UserModelTestCase(unittest.TestCase):
     def test_anonymous_user(self):
         u = AnonymousUser()
         self.assertFalse(u.can(Permission.FOLLOW))
+
+    def test_timestamps(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        self.assertTrue(
+            (datetime.utcnow() - u.member_since).total_seconds() < 3)
+        self.assertTrue(
+            (datetime.utcnow() - u.last_seen).total_seconds() < 3)
+
+    def test_ping(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        time.sleep(2)
+        last_seen_before = u.last_seen
+        u.ping()
+        self.assertTrue(u.last_seen > last_seen_before)
+
+    def test_gravatar(self):
+        u = User(email='john@example.com', password='cat')
+        with self.app.test_request_context('/'):
+            gravatar = u.gravatar()
+            gravatar_256 = u.gravatar(size=256)
+            gravatar_pg = u.gravatar(rating='pg')
+            gravatar_retro = u.gravatar(default='retro')
+        #with self.app.test_request_context('/', base_url='https://example.com'):
+        #    gravatar_ssl = u.gravatar()
+        self.assertTrue('http://www.gravatar.com/avatar/' +
+                        'd4c74594d841139328695756648b6bd6' in gravatar)
+        self.assertTrue('s=256' in gravatar_256)
+        self.assertTrue('r=pg' in gravatar_pg)
+        self.assertTrue('d=retro' in gravatar_retro)
+       # self.assertTrue('https://secure.gravatar.com/avatar/' +
+        #                'd4c74594d841139328695756648b6bd6' in gravatar_ssl)
