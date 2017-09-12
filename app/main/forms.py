75fc28bb856b105d6ed3b1f9bad6d6551a080542
976251185 <976251185@qq.com>
2017-09-12 15:16:28 +0800

    Add profile-editing function and pages

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,45 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField, ValidationError
+from wtforms.validators import Required, Length, Email, Regexp
+from ..models import Role, User
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+#Edit profile by user
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0 ,64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+#Edit profile by administrator
+class EditProfileAdminForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    username = StringField('Username', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Username must have only letters, '
+                                          'numbers, dots or underscores.')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name)
+                             for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+            User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+            User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
