51774185e334bb76334a3af1c80616710ecf7272
976251185 <976251185@qq.com>
2017-09-13 08:21:58 +0800

    Post article on index.

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,23 @@
 from flask import render_template, abort, flash, redirect, url_for
 from flask_login import login_required, current_user
 from . import main
-from ..models import User, Role
-from .forms import EditProfileForm, EditProfileAdminForm
+from ..models import User, Role, Permission, Post
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
 from ..decorators import admin_required
 from datetime import datetime
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+        form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 @main.route('/user/<username>')
 def user(username):
--- app/models.py
+++ app/models.py
@@ -59,6 +59,7 @@ class User(UserMixin, db.Model):
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -140,7 +141,7 @@ class User(UserMixin, db.Model):
             return False
         self.email = new_email
         self.avatar_hash = hashlib.md5(
-            self.email.encode('utf-8')).hexdigext()
+            self.email.encode('utf-8')).hexdigest()
         db.session.add(self)
         return True
 
@@ -159,6 +160,13 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User,%r>' %self.username
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
